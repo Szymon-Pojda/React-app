7e2b9e629243619f328b76bcc923c2f3731d383d
Szymon Pojda <szymonpojda@gmail.com>
2021-12-10 12:54:55 +0000

    wip 15.4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
-import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
+import App from './App';
+
 
 ReactDOM.render(
   <App />,
