cd9dc34a30608962ab0328a5073a66648342cbc8
Szymon Pojda <szymonpojda@gmail.com>
2021-12-13 12:26:48 +0000

    WIP 15.7

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,35 +1,57 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
 import { useState } from 'react';
-import { useEffect } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
   const [columns, setColumns] = useState([
-      { id: 1, title: 'Books', icon: 'book' },
-      { id: 2, title: 'Movies', icon: 'film' },
-      { id: 3, title: 'Games', icon: 'gamepad' }
+    {
+      id: 1,
+      title: 'Books',
+      icon: 'book',
+      cards: [
+        { id: 1, title: 'This is Going to Hurt' },
+        { id: 2, title: 'Interpreter of Maladies' }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Movies',
+      icon: 'film',
+      cards: [
+        { id: 1, title: 'Harry Potter' },
+        { id: 2, title: 'Star Wars' }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Games',
+      icon: 'gamepad',
+      cards: [
+        { id: 1, title: 'The Witcher' },
+        { id: 2, title: 'Skyrim' }
+      ]
+    }
   ]);
 
-  useEffect(() => {
-
-        setTimeout(() => {
-            setColumns([...columns, { id: 4, title: 'Test column'}]);
-        }, 2000);
-
-  }, []);
-  
-    return (
-      <div className={styles.list}>
-          <header className={styles.header}>
-              <h2 className={styles.title}>Things to do<span>soon!</span></h2>
-          </header>
-          <p className={styles.description}>Interesting things I want to check out</p>
-          <section className={styles.columns}>
-           {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
-  </section>
-      </div>
-      
-    );
+  const addColumn = newColumn => {
+    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
   };
-  
-  export default List;
+
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+      {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} />)}
+      </section>
+      <ColumnForm action={addColumn} />
+      
+    </div>
+  );
+};
+
+export default List;
