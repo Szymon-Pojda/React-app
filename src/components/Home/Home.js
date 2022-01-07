d7f8727446e97905b688d231f5ef7de9cfd31268
Szymon Pojda <szymonpojda@gmail.com>
2022-01-07 12:49:05 +0000

    Add 18.1

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import Hero from "../Hero/Hero"
+import List from "../List/List"
+import SearchForm from "../SearchForm/SearchForm"
+
+
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <SearchForm />
+            <List />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import styles from './NotFound.module.scss';
+
+const NotFound = () => {
+    return (
+        <div className={styles.notFound}>
+            <h1 className={styles.title}>404 NOT FOUND</h1>
+        </div>
+    )
+}
+
+export default NotFound;
